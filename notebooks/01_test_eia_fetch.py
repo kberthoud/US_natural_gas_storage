9f1e8547384bc640a01bf5f540fcefacdcc33ddc
Kent Berthoud <kentberthoud@gmail.com>
2025-12-14 15:06:36 -0500

    add processed data with 5 yr avg

--- notebooks/01_test_eia_fetch.py
+++ notebooks/01_test_eia_fetch.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from pathlib import Path
-
+import pandas as pd
 
 #make sure /src is in my path
 PROJECT_ROOT = Path.cwd().parent
@@ -17,7 +17,39 @@ if EIA_KEY is None:
     raise RuntimeError("EIA_API_KEY is not set")
 
 df_storage = fetch_eia_series(series_id, EIA_KEY)
-print(df_storage.head())
+#print(df_storage.head())
 
-data_raw_path = Path("../data/raw")
-df_storage.to_csv(data_raw_path / "lower48_storage_weekly.csv", index=False)
+#data_raw_path = Path("../data/raw")
+#df_storage.to_csv(data_raw_path / "lower48_storage_weekly.csv", index=False)
+
+
+#Calculating the five-year average storage levels
+
+df = df_storage.copy()
+df['week'] = df['period'].dt.isocalendar().week
+df['year'] = df['period'].dt.year
+
+five_year_avg = (
+    df.groupby('week')['value']
+    .rolling(5, min_periods=5).mean()
+    .reset_index(level=0, drop=True)
+)
+
+df['5yr_avg'] = five_year_avg
+df['diff'] = df['value'] - df['5yr_avg']
+
+#print(df.tail())
+#Save this new processed file with 5 yr avg
+
+df_processed = df[
+    ['period', 'value', '5yr_avg', 'diff']
+].copy()
+
+processed_path = Path("../data/processed")
+processed_path.mkdir(parents=True, exist_ok=True)
+
+df_processed.to_csv(
+    processed_path / "lower48_storage_with_5yr_diff.csv",
+    index=False
+)
+
