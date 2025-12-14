0275bb7cdd8f82ad08e0ef026453f33b8f37a933
Kent Berthoud <kentberthoud@gmail.com>
2025-12-14 11:41:29 -0500

    deleting to restructure code

--- notebooks/01_pull_HH_prices.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-HH_front_month = yf.download("NG=F", start="2010-01-01", interval ="1d")
-
-HH_front_month.to_csv("../data/raw/HH_front_month_prices.csv")
